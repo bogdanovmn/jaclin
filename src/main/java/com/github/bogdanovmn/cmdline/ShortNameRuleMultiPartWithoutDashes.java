2905f38ab4177c5576bcbc37ef6eadcb58373fbb
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2020-11-14 18:59:03 +0300

    #9 fix short name generation

---
+++ src/main/java/com/github/bogdanovmn/cmdline/ShortNameRuleMultiPartWithoutDashes.java
@@ -0,0 +1,12 @@
+package com.github.bogdanovmn.cmdline;
+
+import java.util.Optional;
+
+class ShortNameRuleMultiPartWithoutDashes implements ShortNameRule {
+	@Override
+	public Optional<String> shortName(OptionName name) {
+		return Optional.of(
+			String.join("", name.parts())
+		);
+	}
+}
